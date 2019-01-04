1868c9024aa4e31b85a1b5751a725b6e3e67e1b0
Brian Flannery <bflannery66@email.com>
2019-01-04 14:44:29 -0600

    fix path

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+var { Todo } = require('./models/Todo');
+var { User } = require('./models/User');
 
 var app  = express();
 
