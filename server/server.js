b5292a8117f770c19f5ca98f56fdf07abaf62a79
Brian Flannery <bflannery66@email.com>
2019-01-07 07:55:37 -0600

    Add Post /uusers route and token method

--- server/server.js
+++ server/server.js
@@ -94,12 +94,14 @@ app.patch('/todos/:id', (req, res) => {
 
 // USERS
 app.post('/users', (req, res) => {
-  var user = new User({
-    email: req.body.email
-  });
-  user.save().then((doc) => {
-    res.send(doc);
-  }, (err) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body)
+
+  user.save().then(() => (
+    user.generateAuthToken()
+)).then((token) => {
+    res.header('x-auth', token).send(user)
+  }).catch((err) => {
     res.status(400).send(err)
   });
 });
