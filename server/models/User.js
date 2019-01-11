770512e7056b6c0acbaecc7f8e54a5f3c2fc01e9
Brian Flannery <bflannery66@email.com>
2019-01-11 06:44:07 -0600

    add POST /users/login route

--- server/models/User.js
+++ server/models/User.js
@@ -69,6 +69,20 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.recject();
+    }
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        return res ? resolve(user) : reject()
+      });
+    })
+  });
+};
 
 // Hash and Salt
 UserSchema.pre('save', function (next) {
